2a755b8a8ba2e2b834d4156912e5923a89de6d82
sahztrade <s.a.h.z.trade@gmail.com>
2026-06-12 15:36:06 +0330

    Update bot_v3.py

--- bot_v3.py
+++ bot_v3.py
@@ -22,9 +22,9 @@ INTERVAL = os.getenv("INTERVAL", "5m")
 CHECK_MINUTES = int(os.getenv("CHECK_MINUTES", "5"))
 
 MIN_SCORE = 5
-MIN_VOLUME_RATIO = 1.8
-MAX_LONG_RSI = 75
-MIN_SHORT_RSI = 25
+MIN_VOLUME_RATIO = 1.5
+MAX_LONG_RSI = 78
+MIN_SHORT_RSI = 22
 
 LBANK_BASE_URLS = [
     "https://api.lbkex.com",
